5bf729bba77351b66a22d5b2e809cea784d9ecd9
andreathepa <andreadepa32@gmail.com>
2023-05-21 20:19:11 +0200

    Fizz and buzz showed on page

--- js/script.js
+++ js/script.js
@@ -8,27 +8,32 @@
 // Quando stampa multipli di 3 e 5 fizzbuzz
 
 
+let todo_list = document.getElementById('todo')
 
 
 for (let i = 1; i <= 100; i++){
 
     if ( (i % 3 === 0) && (i % 5 === 0)){
 
+        todo_list.innerHTML += `<li>${'FizzBuzz'}</li>`;
         console.log("FizzBuzz")
     }
 
     else if (i % 3 === 0){
 
+        todo_list.innerHTML += `<li>${'Fizz'}</li>`;
         console.log("Fizz");
     } 
     else if (i % 5 === 0){
-        
+
+        todo_list.innerHTML += `<li>${'Buzz'}</li>`;
         console.log("Buzz");
 
     }
     
     else{
 
+        todo_list.innerHTML += `<li>${i}</li>`;
         console.log(i)
     }
 }
