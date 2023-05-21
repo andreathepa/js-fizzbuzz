176173ef5905f33e02da88b33de9db2d4c850de9
andreathepa <andreadepa32@gmail.com>
2023-05-21 21:16:45 +0200

    test

--- js/script.js
+++ js/script.js
@@ -8,64 +8,79 @@
 // Quando stampa multipli di 3 e 5 fizzbuzz
 
 
-let todo_list = document.getElementById('todo')
+ let todo_list = document.getElementById('todo')
+
+
+
+
+ for (let i = 1; i <= 100; i++){
+
+     if ( (i % 3 === 0) && (i % 5 === 0)){
+
+         todo_list.innerHTML += `<li>${'FizzBuzz'}</li>`;
+         console.log("FizzBuzz")
+     }
+
+     else if (i % 3 === 0){
+
+         todo_list.innerHTML += `<li>${'Fizz'}</li>`;
+         console.log("Fizz");
+     } 
+     else if (i % 5 === 0){
+
+         todo_list.innerHTML += `<li>${'Buzz'}</li>`;
+         console.log("Buzz");
+
+     }
+    
+     else{
+
+         todo_list.innerHTML += `<li>${i}</li>`;
+         console.log(i)
+     }
+ }
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
 
 let todo_left = document.getElementById('todo-left')
 
-
+let button = document.getElementById('add-task')
 
 for (let i = 1; i <= 100; i++){
 
     if ( (i % 3 === 0) && (i % 5 === 0)){
 
-        todo_list.innerHTML += `<li>${'FizzBuzz'}</li>`;
         console.log("FizzBuzz")
     }
 
     else if (i % 3 === 0){
 
-        todo_list.innerHTML += `<li>${'Fizz'}</li>`;
+        // todo_left.innerHTML += `<li>${'Fizz'}</li>`;
         console.log("Fizz");
     } 
     else if (i % 5 === 0){
 
-        todo_list.innerHTML += `<li>${'Buzz'}</li>`;
+        // todo_left.innerHTML += `<li>${'Buzz'}</li>`;
         console.log("Buzz");
 
     }
     
     else{
 
-        todo_list.innerHTML += `<li>${i}</li>`;
+        // todo_left.innerHTML += `<li>${i}</li>`;
         console.log(i)
     }
 }
 
+button.addEventListener('click', function(){
+    let text_task = document.getElementById('task').value;
+    let new_task = document.createElement('li');
+    new_task.innerText = text_task;
 
-for (let i = 1; i <= 100; i++){
+    todo_left.append(new_task);
 
-    if ( (i % 3 === 0) && (i % 5 === 0)){
-
-        todo_left.innerHTML += `<li>${'FizzBuzz'}</li>`;
-        console.log("FizzBuzz")
-    }
-
-    else if (i % 3 === 0){
-
-        todo_left.innerHTML += `<li>${'Fizz'}</li>`;
-        console.log("Fizz");
-    } 
-    else if (i % 5 === 0){
-
-        todo_left.innerHTML += `<li>${'Buzz'}</li>`;
-        console.log("Buzz");
-
-    }
     
-    else{
-
-        todo_left.innerHTML += `<li>${i}</li>`;
-        console.log(i)
-    }
-}
+})
 
