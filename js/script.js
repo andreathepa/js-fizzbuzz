6d9e67a3f441f737b00a964181e3cca43e4ccd29
andreathepa <andreadepa32@gmail.com>
2023-05-21 18:34:13 +0200

    ciclo for 1-100

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Creare ciclo for per far apparire in console numeri da 1 a 100
+
+
+// Quando stampa multipli di 3 Fizz
+
+// Quando stampa multipli di 5 buzz
+
+// Quando stampa multipli di 3 e 5 fizzbuzz
+
+
+
+for (let i = 1; i <= 100; i++)
+
+console.log(i)
