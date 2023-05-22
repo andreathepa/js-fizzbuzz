06bc8fc1cf3c4ea48fb0073f357be37156ab5511
andreathepa <andreadepa32@gmail.com>
2023-05-22 09:58:33 +0200

    TEST Bonus 2

--- js/script.js
+++ js/script.js
@@ -10,79 +10,88 @@
 //BONUS 1
 
 
- let todo_list = document.getElementById('todo')
+//  let todo_list = document.getElementById('todo')
 
 
 
 
- for (let i = 1; i <= 100; i++){
+//  for (let i = 1; i <= 100; i++){
 
-     if ( (i % 3 === 0) && (i % 5 === 0)){
+//      if ( (i % 3 === 0) && (i % 5 === 0)){
 
-         todo_list.innerHTML += `<li>${'FizzBuzz'}</li>`;
-         console.log("FizzBuzz")
-     }
+//          todo_list.innerHTML += `<li>${'FizzBuzz'}</li>`;
+//          console.log("FizzBuzz")
+//      }
 
-     else if (i % 3 === 0){
+//      else if (i % 3 === 0){
 
-         todo_list.innerHTML += `<li>${'Fizz'}</li>`;
-         console.log("Fizz");
-     } 
-     else if (i % 5 === 0){
+//          todo_list.innerHTML += `<li>${'Fizz'}</li>`;
+//          console.log("Fizz");
+//      } 
+//      else if (i % 5 === 0){
 
-         todo_list.innerHTML += `<li>${'Buzz'}</li>`;
-         console.log("Buzz");
+//          todo_list.innerHTML += `<li>${'Buzz'}</li>`;
+//          console.log("Buzz");
 
-     }
+//      }
     
-     else{
+//      else{
 
-         todo_list.innerHTML += `<li>${i}</li>`;
-         console.log(i)
-     }
- }
+//          todo_list.innerHTML += `<li>${i}</li>`;
+//          console.log(i)
+//      }
+//  }
 
 
 //////////////////////////////////////////////////////////////////////////////////////
 
 
-let todo_left = document.getElementById('todo-left')
 
-let button = document.getElementById('add-task')
 
-for (let i = 1; i <= 100; i++){
+// let todo_left = document.getElementById('todo-left')
 
-    if ( (i % 3 === 0) && (i % 5 === 0)){
+// let button = document.getElementById('add-task')
 
-        console.log("FizzBuzz")
-    }
+// for (let i = 1; i <= 100; i++){
 
-    else if (i % 3 === 0){
+//     if ( (i % 3 === 0) && (i % 5 === 0)){
 
-        // todo_left.innerHTML += `<li>${'Fizz'}</li>`;
-        console.log("Fizz");
-    } 
-    else if (i % 5 === 0){
+//         console.log("FizzBuzz")
+//     }
 
-        // todo_left.innerHTML += `<li>${'Buzz'}</li>`;
-        console.log("Buzz");
+//     else if (i % 3 === 0){
 
-    }
+//         // todo_left.innerHTML += `<li>${'Fizz'}</li>`;
+//         console.log("Fizz");
+//     } 
+//     else if (i % 5 === 0){
+
+//         // todo_left.innerHTML += `<li>${'Buzz'}</li>`;
+//         console.log("Buzz");
+
+//     }
     
-    else{
+//     else{
 
-        // todo_left.innerHTML += `<li>${i}</li>`;
-        console.log(i)
-    }
-}
+//         // todo_left.innerHTML += `<li>${i}</li>`;
+//         console.log(i)
+//     }
+// }
 
-button.addEventListener('click', function(){
-    let text_task = document.getElementById('task').value;
-    let new_task = document.createElement('li');
-    new_task.innerText = text_task;
+// button.addEventListener('click', function(){
+//     let text_task = document.getElementById('task').value;
+//     let new_task = document.createElement('li');
+//     new_task.innerText = text_task;
 
-    todo_left.append(new_task);
+//     todo_left.append(new_task);
 
     
-})
+// })
+
+
+//////////////////////////////////////////////////////////////////
+
+// BONUS 2
+
+
 
